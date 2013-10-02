980200d757d201f824bb5ea2df4b4c16419544bb
Jeba Singh Emmanuel <jebasingh.emmanuel@gmail.com>
2013-10-02 13:04:46 +0530

    leapmotion works now. after removing lib in requires in leapmotion and ardrone artoos

--- robots.rb
+++ robots.rb
@@ -1,5 +1,29 @@
 #!/usr/bin/env  rvm 1.9 do ruby
-require 'rubygems'
 require 'artoo'
 require 'artoo-leapmotion'
 require 'artoo-ardrone'
+
+connection :leapmotion, :adaptor => :leapmotion, :port => '127.0.0.1:6437'
+device :leapmotion, :driver => :leapmotion
+
+work do
+  on leapmotion, :open => :on_open
+  on leapmotion, :frame => :on_frame
+  on leapmotion, :close => :on_close
+end
+
+def on_open(*args)
+  puts args
+end
+
+def on_frame(*args)
+  frame = args[1]
+  puts frame
+  puts frame.hands
+  puts frame.gestures
+  puts frame.pointables
+end
+
+def on_close(*args)
+  puts args
+end
