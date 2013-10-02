469d6756e1596ad61b6b6a4fd62ad48a3b0cb49d
Jeba Singh Emmanuel <jebasingh.emmanuel@gmail.com>
2013-10-02 12:41:53 +0530

    initial commit struggling with leapmotion require

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'artoo'
+gem 'artoo-leapmotion'
+gem 'artoo-ardrone'
---
+++ robots.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env  rvm 1.9 do ruby
+require 'rubygems'
+require 'artoo'
+require 'artoo-leapmotion'
+require 'artoo-ardrone'
